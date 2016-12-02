298d292681b0beb0dfec80c455e30e4a51aa2246
lgmoneda <lg.moneda@gmail.com>
2016-12-02 03:49:43 -0200

    recommender system

--- pandas_functions.py
+++ pandas_functions.py
@@ -7,6 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib import dates
+import numpy as np
 
 
 ### Corrigindo problemas de encoding
@@ -126,10 +127,14 @@ def build_descriptive_dict(df, how='M'):
 	df = df.set_index('date')
 	df = df.resample(how).sum()
 	descriptive_dict["max"] = int(df["pessoas"].max())
-	if how != "M":
+	print("HOW NA DESCREPTIVE ")
+	print(how)
+	if how != "M" and how != "W" and how != "D":
 		descriptive_dict["max_time"] = df["pessoas"].idxmax().strftime("%H:%M")
 	else:
-		descriptive_dict["max_time"] = df["pessoas"].idxmax()
+		descriptive_dict["max_time"] = df["pessoas"].idxmax().strftime("%d, %b %Y")
+
+
 	
 
 	return descriptive_dict
@@ -175,3 +180,97 @@ def return_selected_stores(query_string, all_stores, conn):
 		else: 
 			table = pd.read_sql("SELECT Lojas.nome, Lojas.categoria FROM Lojas WHERE categoria = \"" + selected_stores + "\";", conn)
 			return list(table["nome"].values)
+
+
+def one_hot_encoding(df, columns, keep=False):
+    """ 
+    Transform categorical data to the one hot encoding representation. 
+    Parameters
+    ----------
+    df: pandas.DataFrame
+        The entire dataframe in which the categorical data is present. 
+    columns: string list
+        A list containing all categorical data columns names.
+    keep: boolean
+        Chooses if the categorical columns will be kept or dropped.
+    Returns
+    -------
+    df: pandas.DataFrame
+        The new dataframe with the categorical columns.
+    ohe_columns: string list
+        A list with the new columns names.
+    Example
+    -------
+    data = pd.DataFrame({"Product Category": ["cellphone", "TV", "refrigerator", "notebook"], 
+                      "Year": ["2008", "2009", "2013", "2016"]})
+    data, categorical_features = onehot_encoding(data, ["Product Category","Year"])
+    """
+    ohe_data = pd.get_dummies(df[columns])
+    ohe_columns = ohe_data.columns
+    
+    if not keep:
+        df = df.drop(columns, axis=1)
+    df = df.join(ohe_data)
+    return df, list(ohe_columns.values)
+
+
+def build_corr(df):
+	df, stores = one_hot_encoding(df, ["nome"], keep = False)
+	df = df.groupby(df["userId"]).sum()
+	#df = df.apply(lambda x : 1 if x > 1 else x)
+	print("Quantidade de users: ")
+	print(len(df))
+	df = df[stores]
+	#df = pd.get_dummies(df["nome"]).set_index(df.index)
+	print(df.head())
+	#sns.set_palette("Blues")
+	#coocc = df.T.dot(df)
+	coocc = df.corr()
+	plt.figure(figsize=(16,16))
+	stores_names = [store[5:] for store in stores]
+	sns.heatmap(coocc, square=True, annot=True, xticklabels=stores_names, yticklabels=stores_names, fmt=".2f", cbar=False, cmap="Blues")
+	#sns.set_palette("Blues")
+	plt.xticks(rotation=90, size=16)	
+	plt.yticks(rotation=0, size=16) 
+
+	plt.savefig("static/images/plots/teste.png", bbox_inches='tight')
+	plt.clf()
+
+	print(df.values)
+	print(coocc)
+	return "static/images/plots/teste.png"
+
+def set_diag(self, values): 
+    n = min(len(self.index), len(self.columns))
+    self.values[[np.arange(n)] * 2] = values
+pd.DataFrame.set_diag = set_diag
+
+def recommend(df, based_on):
+	df, stores = one_hot_encoding(df, ["nome"], keep = False)
+	df = df.groupby(df["userId"]).sum()
+	df = df[stores]
+	coocc = df.T.dot(df)
+	coocc.set_diag(0)
+
+	stores_names = [store[5:] for store in stores]
+	print(stores_names)
+	print(based_on)
+	based_array = np.array([1 if str(store) in based_on else 0 for store in stores_names])
+	print("array da massa")
+	print(based_array)
+
+	print(coocc.index)
+
+	print(np.dot(coocc.as_matrix(), based_array))
+
+	scores = np.dot(coocc.as_matrix(), based_array)
+
+
+
+	recommends = [(x, y) for (y,x) in sorted(zip(stores_names, scores), reverse=False)]
+	recommends = [(x, y) for (x, y) in recommends if x != 0]
+	recommends = sorted(recommends, reverse=True)
+	recommends = ["static/images/logos/"+str(y)+".png" for (x, y) in recommends]
+	print(recommends)
+	return recommends
+
